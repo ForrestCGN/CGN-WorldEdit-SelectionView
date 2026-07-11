be0039c609bf37506237ab554baa83a1e1ca414b
ForrestCGN <127221806+ForrestCGN@users.noreply.github.com>
2026-07-11 10:38:31 +0200

    Wire server config into the mod

--- src/main/java/de/forrestcgn/cgnselectionview/CGNSelectionView.java
+++ src/main/java/de/forrestcgn/cgnselectionview/CGNSelectionView.java
@@ -1,6 +1,7 @@
 package de.forrestcgn.cgnselectionview;
 
 import de.forrestcgn.cgnselectionview.command.SelectionViewCommands;
+import de.forrestcgn.cgnselectionview.config.SelectionViewConfig;
 import de.forrestcgn.cgnselectionview.service.SelectionViewService;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
@@ -9,16 +10,21 @@ import net.neoforged.neoforge.event.RegisterCommandsEvent;
 @Mod(CGNSelectionView.MOD_ID)
 public final class CGNSelectionView {
     public static final String MOD_ID = "cgn_selection_view";
-    public static final String VERSION = "0.2.1";
+    public static final String VERSION = "0.2.2";
 
-    private final SelectionViewService selectionViewService = new SelectionViewService();
+    private final SelectionViewConfig config;
+    private final SelectionViewService selectionViewService;
 
     public CGNSelectionView() {
+        config = new SelectionViewConfig();
+        selectionViewService = new SelectionViewService(config);
+
         NeoForge.EVENT_BUS.addListener(this::registerCommands);
         NeoForge.EVENT_BUS.addListener(selectionViewService::onServerTick);
+        NeoForge.EVENT_BUS.addListener(selectionViewService::onPlayerLogout);
     }
 
     private void registerCommands(RegisterCommandsEvent event) {
-        SelectionViewCommands.register(event.getDispatcher(), selectionViewService);
+        SelectionViewCommands.register(event.getDispatcher(), selectionViewService, config);
     }
 }
